e0fc46ca84c1e017da80479eaa388bcf01b92eef
Liudmyla.Nakonechna <ms.liuda@gmail.com>
2020-03-05 11:27:34 +0200

    custom_waits

--- src/test/java/lesson09/test_siuts/SearchTest.java
+++ src/test/java/lesson09/test_siuts/SearchTest.java
@@ -5,35 +5,36 @@ import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class SearchTest {
     static WebDriver driver;
+    WebDriverWait wait = new WebDriverWait(driver, 10);
 
     @BeforeClass
     public static void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        //driver.get("http://automationpractice.com/index.php");
 
     }
 
     @AfterClass
     public static void tearDown() {
-        driver.quit();
+                driver.quit();
     }
     //SearchTest с одним тестом: открыть сайт, ввести в поиск "Printed Summer Dress" ,
     //нажать поиск, проверить, что появилось три результата и первый совпадает с запросом.
 
     @Test
-    public static void serchTech(){
+    public void searchTech(){
         driver.get("http://automationpractice.com/index.php");
         driver.findElement(By.id("search_query_top")).clear();
         driver.findElement(By.id("search_query_top")).sendKeys("Printed Summer Dress");
-        driver.findElement(By.name("submit_search")).click();
-
+        wait.until(ExpectedConditions.elementToBeClickable(By.name("submit_search"))).click();
 
         Assert.assertTrue(driver.findElements(By.name("product_list grid row")).size()==3 &&
                 driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]")).getText().equals("Printed Summer Dress"));
