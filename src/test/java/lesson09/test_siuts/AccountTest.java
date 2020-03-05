bdeb91971776e4bfaf0c765eb80fad6ada781e8f
Liudmyla.Nakonechna <ms.liuda@gmail.com>
2020-03-05 11:15:09 +0200

    custom_waits

--- src/test/java/lesson09/test_siuts/AccountTest.java
+++ src/test/java/lesson09/test_siuts/AccountTest.java
@@ -13,6 +13,7 @@ package lesson09.test_siuts;
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -28,53 +29,64 @@ public class AccountTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-       // driver.get("http://automationpractice.com/index.php");
 
     }
 
     @AfterClass
     public static void tearDown() {
-
         driver.quit();
     }
 
     @Before
     public void goToStartPage(){
         driver.get("http://automationpractice.com/index.php");
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("login")));
         driver.findElement(By.className("login")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("email")));
         driver.findElement(By.id("email")).sendKeys("ms.liuda+1@gmail.com");
         driver.findElement(By.id("passwd")).sendKeys("Portal123");
         driver.findElement(By.id("SubmitLogin")).click();
     }
+    @After
+    public void logout(){
+        //driver.get("http://automationpractice.com/index.php");
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("logout"))).click();
+    }
+
 
     @Test
     public void openOrderHistoryAndDetails(){
-driver.findElement(By.partialLinkText("Order history and details")).click();
+        WebElement element = driver.findElement(By.cssSelector("#center_column > div > div:nth-child(1) > ul > li:nth-child(1) > a > span"));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+element.click();
         Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?controller=history"));
     }
 
     @Test
     public void openMyAddresses(){
-        driver.findElement(By.partialLinkText("My addresses")).click();
+        WebElement element = driver.findElement(By.cssSelector("#center_column > div > div:nth-child(1) > ul > li:nth-child(3) > a > span"));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
         Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?controller=addresses"));
     }
 
     @Test
     public void openMyCreditSlips(){
-        driver.findElement(By.partialLinkText("My credit slips")).click();
+        WebElement element = driver.findElement(By.cssSelector("#center_column > div > div:nth-child(1) > ul > li:nth-child(2) > a > span"));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
         Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?controller=order-slip"));
     }
 
     @Test
     public void openMyPersonalInformation(){
-        driver.findElement(By.partialLinkText("My personal information")).click();
+        WebElement element = driver.findElement(By.cssSelector("#center_column > div > div:nth-child(1) > ul > li:nth-child(4) > a > span"));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
         Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?controller=identity"));
     }
 
     @Test
-    public void openMyWishlist(){
-        driver.findElement(By.partialLinkText("My wishlists")).click();
+    public void openMyWhilst(){
+        WebElement element = driver.findElement(By.cssSelector("#center_column > div > div:nth-child(2) > ul > li > a > span"));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
         Assert.assertTrue(driver.getCurrentUrl().equals("http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist"));
     }
 }
